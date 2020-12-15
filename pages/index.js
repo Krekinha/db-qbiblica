6065d5bff16eef806871230c7c432a70e2512033
Dionison <krekmg@gmail.com>
2020-12-15 15:53:31 -0300

    add página /sobre - uso do Link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
-import { useState } from 'react'
+import Link from 'next/link';
 
 function Home() {
     return (
-    <div><h1>Home</h1></div>
+    <div>
+        <h1>Home</h1>
+        <Link href="/sobre">
+            <a>Sobre</a>
+        </Link>
+    </div>
     )
 }
 
---
+++ pages/sobre.js
@@ -0,0 +1,12 @@
+function Sobre() {
+    return (
+    <div>
+        <h1>Sobre</h1>
+        <Link href="/">
+            <a>Home</a>
+        </Link>
+    </div>
+    )
+}
+
+export default Sobre
